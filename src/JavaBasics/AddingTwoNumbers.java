95918d531d5da5c8154c63aea2df4239a85838e9
rolando926 <rolando_colon@homedepot.com>
2017-05-02 12:14:14 -0400

    Adding program for adding two numbers and concat two strings.

---
+++ src/JavaBasics/AddingTwoNumbers.java
@@ -0,0 +1,28 @@
+package JavaBasics;
+
+/**
+ * Created by RXC8414 on 5/2/2017.
+ */
+public class AddingTwoNumbers {
+    public static void main(String[] args) {
+        // Declaring variable and initializing them to values
+        int number1 = 10;
+        int number2 = 20;
+
+        // Printing the content of both variables
+        System.out.println(number1 + number2);
+
+        // I can always change the values during RunTime
+        number1 = 40;
+        System.out.println(number1 + number2);
+
+        System.out.println("--------------------");
+
+        // Create a print statement that adds two strings
+        String str1 = "Rolando";
+        String str2 = "Colon";
+        System.out.println(str1 + " " + str2);
+
+
+    }
+}
