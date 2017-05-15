419f07186743873fbe3678234276fdf8c9eb7368
rolando926 <rolando_colon@homedepot.com>
2017-05-15 11:55:48 -0400

    Adding the a few more regex examples including string rules, email rules and password.

--- src/ClassesObjects/Regex.java
+++ src/ClassesObjects/Regex.java
@@ -15,4 +15,78 @@ public class Regex {
             System.out.println(name+" is not a valid name.");
         }
     }
+
+    // Create a method that receives an array of Strings and applies regex to them
+    public void applyRgxArrayString(String[] names, int rule){
+        String[] regexRules = new String[4];
+        String ruleToApply;
+
+        // Regex rule for names less/equal than 5 characters
+        regexRules[0] = ".{5}";
+
+        // Regex rule for names greater/equal than 6 but less/equal than 7
+        regexRules[1] = ".{6,7}";
+
+        // Regex rule for names that start with the letter B
+        regexRules[2] = "^B.*";
+
+        // Regex rule for names that end with 'y'
+        regexRules[3] = ".*y$";
+
+        // Conditional for rules
+        if(rule == 1){
+            ruleToApply = regexRules[0];
+        }
+        else if(rule == 2){
+            ruleToApply = regexRules[1];
+        }
+        else if(rule == 3){
+            ruleToApply = regexRules[2];
+        }
+        else if(rule == 4){
+            ruleToApply = regexRules[3];
+        }
+        else{
+            System.out.println("This rule does not exist. Please select rules 1-4.");
+            return;
+        }
+
+        // Another way of doing a conditional statement
+        switch(rule){
+            case 1:
+                ruleToApply = regexRules[0];
+                break;
+            case 2:
+                ruleToApply = regexRules[1];
+                break;
+            case 3:
+                ruleToApply = regexRules[2];
+                break;
+            case 4:
+                ruleToApply = regexRules[3];
+                break;
+            default:
+                System.out.println("This rule does not exist. Please select rules 1-4.");
+                return;
+        }
+
+        // Print out the valid names
+        for (String temp:names) {
+            if(temp.matches(ruleToApply)){
+                System.out.println(temp);
+            }
+        }
+
+
+    }
+
+
+    public void validateEmail(String[] emails){
+        String regexRuleApply = "^[^@]{3}[^@]*@[a-zA-Z]+((.com)|(.org)|(.net)|(.gov))$";
+        for (String temp:emails) {
+            if(temp.matches(regexRuleApply)){
+                System.out.println(temp);
+            }
+        }
+    }
 }
--- src/ClassesObjects/RegexMain.java
+++ src/ClassesObjects/RegexMain.java
@@ -4,15 +4,31 @@ package ClassesObjects;
  * Created by RXC8414 on 5/12/2017.
  */
 public class RegexMain {
+
+
     public static void main(String[] args) {
-        String firstName = args[0];
-        String lastName = args[1];
-        String fullName = firstName + " " + lastName;
+        String[] fruits = {"Apple","Grape","Strawberry","Berry","Banana","Peach","Melon","Mango"};
+        String[] emails = {"your.name@gmail.com",
+                        "abc.dfg@yahoo.com",
+                        "@@@@something.com",
+                        "AB@mycompany.org",
+                        "abcd@homedepot.info",
+                        "your!!!Name123@homedepot.com"};
+
+        //String firstName = args[0];
+        //String lastName = args[1];
+        //String fullName = firstName + " " + lastName;
 
         // First thing is to do instantiation
         Regex myRegex = new Regex();
 
         // Call my first method - Verify if valid full name.
-        myRegex.verifyFullName(fullName);
+        //myRegex.verifyFullName(fullName);
+
+        // Validate Regex rules
+        //myRegex.applyRgxArrayString(fruits,-1);
+
+        //Validate Email Regex Rule
+        myRegex.validateEmail(emails);
     }
 }
